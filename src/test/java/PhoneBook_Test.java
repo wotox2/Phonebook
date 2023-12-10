d9206c84b996fcb9aa92c039ebcfa1e83935c00d
aryanoor <aryan.nourbakhsh@gmail.com>
2023-12-10 05:41:24 +0330

    Create a class for our unit tests

---
+++ src/test/java/PhoneBook_Test.java
@@ -0,0 +1,11 @@
+/**
+ * writing unit tests for each method of classes
+ * If you have no idea about how to write unit tests, you can search the internet or check your first assignment
+ * After each unit test you've written, please commit the changes and push to your repo
+
+ * Hint: Your test coverage should be 100%
+ * Attention:  We would check your commits and it will affect your score
+ */
+public class PhoneBook_Test {
+
+}
