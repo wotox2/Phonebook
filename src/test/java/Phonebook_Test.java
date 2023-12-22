f39b617029f47bc68cadb9b1a4135223a85b4298
wotox2 <49289130+wotox2@users.noreply.github.com>
2023-12-22 11:23:23 +0330

    Test For  Updating Contact Name

--- src/test/java/Phonebook_Test.java
+++ src/test/java/Phonebook_Test.java
@@ -6,15 +6,16 @@ import java.util.ArrayList;
 public class Phonebook_Test {
 
 
-    // Test For Getting  Contact Not Found
+    // Test For  Updating Contact Name
     @Test
-    public void testGetContactNotFound() {
+    public void testUpdateContactName() {
         Phonebook phonebook = new Phonebook();
-        Person person = new Person("Bob", "09123456789");
+        Person person = new Person("Eve", "09123456789");
         phonebook.addContact(person);
 
-        int result = phonebook.getContact("Charlie");
+        int result = phonebook.updateContactName("Eve", "Eva");
 
-        assertEquals(0, result);
+        assertEquals(1, result);
+        assertEquals("Eva", person.getName());
     }
 }
